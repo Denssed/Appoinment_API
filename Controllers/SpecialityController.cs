a61c6a6ed32c55ae161eb2bda221d4ba2e5e62e6
Denssed <rodriguezp.jeremys@gmail.com>
2023-06-08 12:41:33 -0500

    Add Speciality Controller

---
+++ Controllers/SpecialityController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Appoiment_API.Controllers
+{
+    public class SpecialityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
